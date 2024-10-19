6f145c7126102bbbcdac4a4dc0a796f162183752
dunglv202 <dunglv202@gmail.com>
2024-10-19 15:40:04 +0700

    add loading animation homepage

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -2,7 +2,7 @@ import MovieGrid from '@/components/MovieGrid/MovieGrid'
 import { Category } from '@/models/category'
 import { Movie } from '@/models/movie'
 import { getMovies } from '@/services/movie-service'
-import { Divider, Typography } from 'antd'
+import { Divider, Spin, Typography } from 'antd'
 import { useEffect, useState } from 'react'
 import CategoryGrid from './CategoryGrid/CategoryGrid'
 
@@ -10,20 +10,26 @@ const HomePage = () => {
   const [category, setCategory] = useState<Category>()
   const [nowShowing, setNowShowing] = useState<Movie[]>([])
   const [comingSoon, setComingSoon] = useState<Movie[]>([])
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     const fetchMovies = async () => {
-      const nowShowing = await getMovies({
-        category: category?.code,
-        nowShowing: true,
-      })
-      setNowShowing(nowShowing.content)
+      setLoading(true)
+      try {
+        const nowShowing = await getMovies({
+          category: category?.code,
+          nowShowing: true,
+        })
+        setNowShowing(nowShowing.content)
 
-      const comingSoon = await getMovies({
-        category: category?.code,
-        comingSoon: true,
-      })
-      setComingSoon(comingSoon.content)
+        const comingSoon = await getMovies({
+          category: category?.code,
+          comingSoon: true,
+        })
+        setComingSoon(comingSoon.content)
+      } finally {
+        setLoading(false)
+      }
     }
 
     fetchMovies()
@@ -34,22 +40,24 @@ const HomePage = () => {
       <section style={{ marginBottom: 30 }}>
         <CategoryGrid onSelect={setCategory} />
       </section>
-      <section>
-        <Divider>
-          <Typography.Title level={2} style={{ fontSize: 17, color: 'var(--app-color-primary)' }}>
-            Phim đang chiếu
-          </Typography.Title>
-        </Divider>
-        <MovieGrid movies={nowShowing} />
-      </section>
-      <section>
-        <Divider>
-          <Typography.Title level={2} style={{ fontSize: 17, color: 'var(--app-color-primary)' }}>
-            Phim sắp chiếu
-          </Typography.Title>
-        </Divider>
-        <MovieGrid movies={comingSoon} />
-      </section>
+      <Spin spinning={loading}>
+        <section>
+          <Divider>
+            <Typography.Title level={2} style={{ fontSize: 17, color: 'var(--app-color-primary)' }}>
+              Phim đang chiếu
+            </Typography.Title>
+          </Divider>
+          <MovieGrid movies={nowShowing} />
+        </section>
+        <section>
+          <Divider>
+            <Typography.Title level={2} style={{ fontSize: 17, color: 'var(--app-color-primary)' }}>
+              Phim sắp chiếu
+            </Typography.Title>
+          </Divider>
+          <MovieGrid movies={comingSoon} />
+        </section>
+      </Spin>
     </>
   )
 }
